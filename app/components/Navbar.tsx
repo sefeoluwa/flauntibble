0adb6e11ec5ab39a2b471bb6fb220f97e73a69c0
sefeoluwa <sefeoluwa01@gmail.com>
2023-12-29 19:31:25 +0100

    Refactor Navbar component to include logo image

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,21 @@
+import Image from "next/image"
+import Link from "next/link"
+import logo_black from 'public/logo_black.png'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flexBetween navbar">
+        <div className="flex-1 flex-start gap-10">
+            <Link href= '/'>
+                <Image 
+                    src='/logo_black.png'
+                    width={115}
+                    height={43}
+                    alt='flauntibble'
+                />
+            </Link>
+        </div>
+    </nav>
   )
 }
 
