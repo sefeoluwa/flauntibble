4d86deca6793c461e73b41904e1173464ab7672b
sefeoluwa <sefeoluwa01@gmail.com>
2023-12-30 06:52:54 +0100

    Add footer column component and update logo and description

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,22 @@
+import { footerLinks } from "@/constants";
 import Image from "next/image"
+import Link from "next/link"
+
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+}
+
+const FooterColumn = ({ title, links } : ColumnProps) => {
+  return (
+    <div className="footer_column">
+      <h4 className="font-semibold">{title}</h4>
+      <ul className="flex flex-col gap-2 font-normal">
+        {links.map((link) => <Link href='/' key={link}>{link}</Link>)}
+      </ul>
+    </div>
+  );
+}
 
 const Footer = () => {
   return (
@@ -6,17 +24,19 @@ const Footer = () => {
       <div className="flex flex-col gap-12 w-full">
         <div className="flex items-start flex-col">
           <Image
-          src='/logoColor.png'
-          width={115}
-          height={38}
-          alt="Flauntibble"
-          >
-
-          </Image>
+            src='/logoColor.png'
+            width={115}
+            height={38}
+            alt="Flauntibble"
+          />
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">Flauntibble is the world's leading community for creatives to share, grow, and get hired.</p>
+        </div>
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links}/>
         </div>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
