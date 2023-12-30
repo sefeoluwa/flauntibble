cbeab2cd3c1af4fffa9b3b14a6dc2de9bdde83e1
sefeoluwa <sefeoluwa01@gmail.com>
2023-12-30 05:54:28 +0100

    added dynamic navigation links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
 import logo_black from '../logo_black.png'
+import { NavLinks } from "@/constants"
 
 const Navbar = () => {
   return (
     <nav className="flexBetween navbar">
-        <div className="flex-1 flex-start gap-10">
+        <div className="flex-1 flex flex-start gap-10">
             <Link href= '/'>
                 <Image 
                     src='/logo_black.png'
@@ -14,8 +15,12 @@ const Navbar = () => {
                     alt='flauntibble'
                 />
             </Link>
-            <ul className="xl:flex hidden text-small gap-7">
-               
+            <ul className="xl:flex items-center hidden text-small gap-7">
+               {NavLinks.map((link) => (
+                <Link href={link.href} key={link.key}>
+                  {link.text}
+                </Link>
+               ))}
             </ul>
         </div>
     </nav>
