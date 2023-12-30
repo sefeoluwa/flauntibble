a021b41d9634541b898b3cd69ca6ea3705bba0bc
sefeoluwa <sefeoluwa01@gmail.com>
2023-12-30 06:39:54 +0100

    Add logo to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,21 @@
+import Image from "next/image"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="flexStart">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+          src='/logoColor.png'
+          width={115}
+          height={38}
+          alt="Flauntibble"
+          >
+
+          </Image>
+        </div>
+      </div>
+    </footer>
   )
 }
 
