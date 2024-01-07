0d6d836c6e44dd3f56424bd91dbaba0d7d28adea
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-07 22:06:00 +0100

    Add auth state

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+'use client'
+
+import {useState, useEffect} from 'react'
+import { getProviders, signIn } from 'next-auth/react'
+
+type Providers = {
+   
+}
 
 const AuthProviders = () => {
+  const [providers, setProviders] = useState<Providers | null>(null)
   return (
     <div>AuthProviders</div>
   )
