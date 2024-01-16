b010d0283f8ac9c8e9e6dec435a6b370c212b8dc
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-16 21:00:24 +0100

    Fix Navbar async function and add user photo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,8 @@ import AuthProviders from "./AuthProviders"
 import { getCurrentUser } from "@/lib/session"
 
 const Navbar = async () => {
-  const session = await getCurrentUser;
+  const session = await getCurrentUser();
+
 
   return (
     <nav className="flexBetween navbar">
@@ -25,15 +26,25 @@ const Navbar = async () => {
                 </Link>
                ))}
             </ul>
-        </div>
+        </div> 
         <div className="flexCenter gap-4">
-          {session ? (
+          {session?.user ? (
             <>
-            UserPhoto
-            <Link href='/create-project'>
+        {session?.user?.image 
+        &&
+        (
+          <Image 
+          src={session.user.image}
+          width={40}
+          height={40}
+          className="rounded-full"
+          alt={session.user.name}
+          /> )}
+          
+          <Link href='/create-project'>
             Share work
             </Link>
-            </>
+           </>
           ) : (
             <AuthProviders />
           )}
