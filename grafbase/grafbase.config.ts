4d8726b9ab2cd72d0fb1b67b553993c476717a91
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-23 18:40:35 +0100

    the grafbase server works here

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,64 @@
+// import { g, config, auth } from '@grafbase/sdk'
+
+
+// const User = g.model('User', {
+//   name: g.string().length({ min: 2, max: 20 }),
+//   email: g.string().unique(),
+//   avatarUrl: g.url(),
+//   description: g.string().optional(),
+//   githubUrl: g.url().optional(),
+//   linkedInUrl: g.url().optional(),
+//   projects: g.relation(() => Projects).list().optional(),
+// })
+
+// const Projects = g.model('Project', {
+//   title: g.string().length({ min: 3 }),
+//   description: g.string(),
+//   image: g.url(),
+//   liveSiteurl: g.url(),
+//   githubUrl: g.url(),
+//   category: g.string().search(),
+//   createdBy: g.relation(() => User)
+// })
+
+// export default config({
+//   schema: g
+  
+// })
+
+
+import { config, connector, g } from '@grafbase/sdk'
+
+const mongodb = connector.MongoDB('MongoDB', {
+  url: g.env('MONGO_ATLAS_URL'),
+  apiKey: g.env('MONGO_API_KEY'),
+  dataSource: g.env('MONGO_DATASOURCE'),
+  database: g.env('MONGO_DATABASE')
+})
+
+g.datasource(mongodb)
+const User = mongodb.model('User', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.ref('Projects').list().optional(),
+})
+
+  const Projects = mongodb.model('Projects', {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteurl: g.url(),
+    githubUrl: g.url(),
+    category: g.string(),
+    createdBy: g.ref('User')
+  })
+
+  .collection('users')
+
+  export default config({
+    schema: g,
+  })
