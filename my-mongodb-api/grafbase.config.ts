0a0fdc655e8b36f0731debc91e98d8a2eaf1f71e
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-20 18:30:51 +0100

    added main grafbase config to mongodb flder

--- my-mongodb-api/grafbase.config.ts
+++ my-mongodb-api/grafbase.config.ts
@@ -1,42 +1,50 @@
-import { graph, config } from '@grafbase/sdk'
+import { config, connector, g, auth } from '@grafbase/sdk'
 
-// Welcome to Grafbase!
-//
-// Configure authentication, data sources, resolvers and caching for your GraphQL API.
-
-const g = graph.Standalone()
-
-// Data Sources - https://grafbase.com/docs/connectors
-//
-// const pg = connector.Postgres('pg', { url: g.env('DATABASE_URL') })
-// g.datasource(pg)
-
-// Resolvers - https://grafbase.com/docs/resolvers
-//
-// g.query('helloWorld', {
-//   returns: g.string(),
-//   resolver: 'hello-world',
-// })
-
-export default config({
-  graph: g,
-  // Authentication - https://grafbase.com/docs/auth
-  auth: {
-    // OpenID Connect
-    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
-    // providers: [oidc],
-    rules: (rules) => {
-      rules.public()
-    },
-  },
-  // Caching - https://grafbase.com/docs/graphql-edge-caching
-  // cache: {
-  //   rules: [
-  //     {
-  //       types: ['Query'], // Cache everything for 60 seconds
-  //       maxAge: 60,
-  //       staleWhileRevalidate: 60
-  //     }
-  //   ]
-  // }
+const mongodb = connector.MongoDB('MongoDB', {
+  url: g.env('MONGO_ATLAS_URL'),
+  apiKey: g.env('MONGO_API_KEY'),
+  dataSource: g.env('MONGO_DATASOURCE'),
+  database: g.env('MONGO_DATABASE')
 })
+
+g.datasource(mongodb)
+const User = mongodb.model('User', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.string(),
+}).auth((rules) => {
+  rules.public().read()
+})
+
+  const Projects = mongodb.model('Projects', {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteurl: g.url(),
+    githubUrl: g.url(),
+    category: g.string(),
+    createdBy: g.string(),
+  }).auth((rules) => {
+    rules.public().read(),
+    rules.private().create().delete().update();
+  })
+
+  .collection('users')
+
+
+  const jwt = auth.JWT({
+    issuer: 'grafbase',
+    secret: g.env('NEXTAUTH_SECRET')
+  })
+
+  export default config({
+    schema: g,
+    auth: {
+      providers: [jwt],
+      rules: (rules) => rules.private(),
+    }
+  })
